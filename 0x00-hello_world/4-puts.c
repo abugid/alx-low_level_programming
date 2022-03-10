1aacd3abd81dfe3cb3e7a6a85c34aafb3ddeac16
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-10 14:12:58 +0300

    fix 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * *main - Entry point
+ * *
+ * *Return: Always 0 (Success)
+ * */
 
-int main(void){
-	char str1[51] = "Programming is like building a multilingual puzzle";
-	puts(str1);
-	return (0);
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
