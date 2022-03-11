e5956b31825ffb530f7f3f11448012f7f5d88bba
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-11 09:36:37 +0300

    second task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+int lastDigit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+lastDigit = n % 10;
+printf("Last digit of %d is %d and is ", n, lastDigit);
+
+if (lastDigit > 5)
+{
+printf("greater than 5\n");
+}
+else if (lastDigit == 0)
+{
+printf("0\n");
+}
+else
+{
+printf("less than 6 and not 0\n");
+}
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0 print the sign of a random integer
+1 print the last digit of a random integer
