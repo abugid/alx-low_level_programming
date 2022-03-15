9446e516098f5e903df649dd883b5583c227b541
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-15 19:10:51 +0300

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-/** 
- * print_alphabet
+/**
+ * print_alphabet - prints the alphabet
  * prints the alphabet
  * Return: void
  */
 void print_alphabet(void)
 {
-    int i, j = 97;
-    for (i = 0; i < 26; i++)
-    {
-        _putchar(j);
-        j++;
-    }
-    _putchar('\n');
+int i, j = 97;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(j);
+j++;
+}
+_putchar('\n');
 }
