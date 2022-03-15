2a66677f0e492ed6ced1361dba72c920cb0588ad
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-15 19:48:40 +0300

    fourth task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is lowercase
+ * @c: character to be checked
+ * Return: 1 if lowercase 0 if not
+ */
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0 prints _putchar
 1 prints the alphabet in lowercase
 2 print the alphabet ten times
+3 checks if a character is lower case
