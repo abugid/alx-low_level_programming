aa6104de1cc6e84b79472a76da297ff23612b881
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-15 22:34:28 +0300

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,13 +7,15 @@
 */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		for (n = 0; n <= 97; n++)
-			printf("%d", n);
+		for (i = n; i <= 97; i++)
+			printf("%d, ", i);
 	}
 	else
-		for (n = 0; n > 98; n--)
-			printf("%d", n);
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
 	printf("98\n");
 }
