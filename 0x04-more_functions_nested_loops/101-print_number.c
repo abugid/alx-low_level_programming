b8f2f2f3c284eb23f81514d7e8c15cb5730bee2c
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-17 18:36:59 +0300

    changed algorithm

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,38 +4,23 @@
  * print_number - prints an integer
  * @n: number to be printed
  */
-
 void print_number(int n)
 {
-int numDigits, denm, j, k, n1;
-if (n == 0)
-{
-_putchar('0');
-}
-numDigits = 0;
-denm = 1;
+	unsigned int n1;
 
-if (n < 0)
-{
-n *= -1;
-_putchar('-');
-}
-n1 = n;
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
 
-while (n > 0)
-{
-n = n / 10;
-numDigits++;
-}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
 
-for (j = 0; j < numDigits; j++)
-{
-denm *= 10;
-}
-for (k = 0; k < numDigits; k++)
-{
-denm /= 10;
-_putchar(n1 / denm + '0');
-n1 = n1 % denm;
-}
+	_putchar((n1 % 10) + '0');
 }
