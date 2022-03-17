c5f183d393fc1774dc36bd55586e91de3db875d1
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-17 14:47:56 +0300

    fifth task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 through 9
+ *
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1 checks for digits
 2 multiplies two numbers
 3 print numbers 0-9
+4 print the digits without 2 & 4
