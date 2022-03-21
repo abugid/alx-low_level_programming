a4b5b606e56e200a63304344f4ea5d584376cbe8
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-21 12:33:58 +0300

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value to 98
+ * @n: pointer that points to a value
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0 resets value using pointer
