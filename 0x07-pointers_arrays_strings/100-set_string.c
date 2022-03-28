ec615945384f83689f5752b1373067cd32cc4cfa
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-28 13:20:18 +0300

    ninth task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to be set to
+ * @to: pointer to be set
+ * Return: None
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -6,3 +6,4 @@
 5 looks for a substring in a string
 6 prints a chessboard
 7 prints the sum of a diagonal matrix
+8 sets the value of a pointer to a char
