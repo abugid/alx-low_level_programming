b3818e50c9ce2eba8da08706ae922be7172abe5c
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-03-28 12:52:21 +0300

    hot fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strstr - search for a substring in a string
@@ -37,5 +38,5 @@ char *_strstr(char *haystack, char *needle)
 		i++;
 
 	}
-	return (0);
+	return (NULL);
 }
