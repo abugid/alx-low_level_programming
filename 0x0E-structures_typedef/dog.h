6cb6edb2e5be0a99fb057f004cc3d7d27deb1e68
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-04-11 20:25:50 +0300

    modified header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,10 @@
  * @age: integer
  * @owner: character string
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 #endif
