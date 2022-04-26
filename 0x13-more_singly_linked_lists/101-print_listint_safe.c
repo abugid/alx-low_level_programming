43cef7aa9b849f64a1667a33895f6a24d73e5194
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-04-26 05:14:52 +0300

    major fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,68 +1,88 @@
 #include "lists.h"
-/**
- *add_nodeaddr - A function that mallocs a new space for a list
- *@head: A double pointer that points to my second list header, head_2.
- */
-addr_list *add_nodeaddr(addr_list **head, const void *addr)
-{
-	addr_list *new_node;
+#include <stdio.h>
 
-	new_node = malloc(sizeof(addr_list));
-	if (new_node == NULL)
-	{
-		free_listaddr(*head);
-		exit (98);
-	}
-	new_node->addr = addr;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
- *free_listaddr - a function that frees a linked list
- *@head: a pointer variable that points to the start of a newly made list
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
-void free_listaddr(addr_list *head)
+size_t looped_listint_len(const listint_t *head)
 {
-	addr_list *current_node;
-	while (head != NULL)
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		current_node = head;
-		head = head->next;
-		free(current_node);
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
+
+	return (0);
 }
+
 /**
- *print_listint_safe - a function that prints the node and the
- *number within a node,
- *Return: the number of nodes
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	addr_list *current_NewNode;
-	addr_list *head_2;
-	size_t i;
+	size_t nodes, index = 0;
 
-	i = 0;
-	head_2 = NULL;
-	while (head != NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		current_NewNode = head_2;
-		while (current_NewNode != NULL)
+		for (; head != NULL; nodes++)
 		{
-			if (head == current_NewNode->addr)
-			{
-				printf("->[%p] %d\n", (void *)head, head->n);
-				free_listaddr(head_2);
-				return (i);
-			}
-			current_NewNode = current_NewNode->next;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		add_nodeaddr(&head_2, head);
-		head = head->next;
-		i++;
 	}
-	free_listaddr(head_2);
-	return (i);
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,68 +1,96 @@
 #include "lists.h"
-/**
- *add_nodeaddr - A function that mallocs a new space for a list
- *@head: A double pointer that points to my second list header, head_2.
- */
-addr_list *add_nodeaddr(addr_list **head, const void *addr)
-{
-	addr_list *new_node;
 
-	new_node = malloc(sizeof(addr_list));
-	if (new_node == NULL)
-	{
-		free_listaddr(*head);
-		exit (98);
-	}
-	new_node->addr = addr;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 /**
- *free_listaddr - a function that frees a linked list
- *@head: a pointer variable that points to the start of a newly made list
+ * looped_listint_count - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
-void free_listaddr(addr_list *head)
+size_t looped_listint_count(listint_t *head)
 {
-	addr_list *current_node;
-	while (head != NULL)
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		current_node = head;
-		head = head->next;
-		free(current_node);
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
+
+	return (0);
 }
+
 /**
- *print_listint_safe - a function that prints the node and the
- *number within a node,
- *Return: the number of nodes
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	addr_list *current_NewNode;
-	addr_list *head_2;
-	size_t i;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	i = 0;
-	head_2 = NULL;
-	while (head != NULL)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		current_NewNode = head_2;
-		while (current_NewNode != NULL)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
-			if (head == current_NewNode->addr)
-			{
-				printf("(nil), (nil)");
-				free_listaddr(head_2);
-				return (i);
-			}
-			current_NewNode = current_NewNode->next;
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		add_nodeaddr(&head_2, head);
-		head = head->next;
-		i++;
 	}
-	free_listaddr(head_2);
-	return (i);
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
 }
